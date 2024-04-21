212f7277d88cc870ab29470602601387a0d1ecf7
Dasharath Naik <dasharathnaik4851@gmail.com>
2024-04-21 23:37:51 +0530

    Created HomeViewModel

---
+++ PetCare Connect/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewModel.swift
+//  PetCare Connect
+//
+//  Created by Dasharath Naik on 21/04/24.
+//
+
+import Foundation
+
+class HomeViewModel {
+    var petsData: [Pets] = []
+    var configData: ConfigModel = ConfigModel()
+    
+    func getPetsData(completion: @escaping (()->Void)) {
+        if let data = Helper.getDummyData(json: .Pets, type: PetsModel.self) {
+            self.petsData = data.pets ?? [Pets]()
+            completion()
+        }
+    }
+    
+    func getConfigData(completion: @escaping (()->Void)) {
+        if let data = Helper.getDummyData(json: .Config, type: ConfigModel.self) {
+            self.configData = data
+            completion()
+        }
+    }
+}
+
+struct ConfigModel: Codable {
+    var settings: SettingsModel? = nil
+}
+
+struct SettingsModel: Codable {
+    let isChatEnabled: Bool?
+    let isCallEnabled: Bool?
+    let workHours: String?
+}
